6ce6c0b43f6001ddb2da24dc6551c34ca80a7e8d
kelvincao2002 <43891826+kelvincao2002@users.noreply.github.com>
2018-11-15 10:52:31 -0800

    Initial commit

---
+++ Digits.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+
+
+private ArrayList<Integer> digitList;
+
+public Digits()
+{
+}
+public class Digits
+{
+ 
+ public Digits(int num)
+ {
+    digitList = new ArrayList<Integer>();
+   {
+      
+}
+}
+}
